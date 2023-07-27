45752d6c7cb4fc4a6661915a3af4f9ff12500f0d
kongshier <2927527234@qq.com>
2023-07-27 14:52:50 +0800

    新增用户自增表，用户注册编号自增

---
+++ src/main/java/com/shier/shierbi/mapper/UserCodeMapper.java
@@ -0,0 +1,18 @@
+package com.shier.shierbi.mapper;
+
+import com.shier.shierbi.model.entity.UserCode;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Shier
+* @description 针对表【user_code(用户)】的数据库操作Mapper
+* @createDate 2023-07-19 16:00:09
+* @Entity com.shier.shierbi.model.entity.UserCode
+*/
+public interface UserCodeMapper extends BaseMapper<UserCode> {
+
+}
+
+
+
+
---
+++ src/main/java/com/shier/shierbi/model/entity/UserCode.java
@@ -0,0 +1,46 @@
+package com.shier.shierbi.model.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 用户
+ * @author Shier
+ * @TableName user_code
+ */
+@TableName(value ="user_code")
+@Data
+public class UserCode implements Serializable {
+    /**
+     * id
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 用户id
+     */
+    private Long userId;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 是否删除
+     */
+    @TableLogic
+    private Integer isDelete;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
